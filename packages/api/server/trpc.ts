47cfc763e455025a36225e9445c6a5da227cd332
Vishnu Kumar <vishnukumart96@gmail.com>
2023-11-17 10:28:13 +0530

    fix : using trpc@server in a non server environment

--- packages/api/server/trpc.ts
+++ packages/api/server/trpc.ts
@@ -3,7 +3,7 @@ import { initTRPC } from "@trpc/server";
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create()
+const t = initTRPC.create({allowOutsideOfServer: true})
 // Base router and procedure helpers
 export const router = t.router;
 export const procedure = t.procedure;
