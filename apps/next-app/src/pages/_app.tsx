1e0a67dbb05a0512b2cacc69c2fe4b619cd650b7
Vishnu Kumar <vishnukumart96@gmail.com>
2023-11-17 10:26:18 +0530

    integrate tRPC

--- apps/next-app/src/pages/_app.tsx
+++ apps/next-app/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppType } from 'next/app';
+import { trpc } from 'api';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+export default trpc.withTRPC(MyApp);
---
+++ packages/api/index.ts
@@ -0,0 +1,3 @@
+export * from "./server/trpc"
+export * from "./server/routers/_app"
+export * from "./utils/trpc"
