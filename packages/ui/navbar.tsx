cdf2970e1eade6a1d70721fb0eade0176a5b16f8
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-01 13:56:12 +0530

    add DarkMode Toggler

--- packages/ui/navbar.tsx
+++ packages/ui/navbar.tsx
@@ -1,8 +1,11 @@
+import { DarkModeToggle } from "./darkModeToggle";
+
 export function Navbar(){
 
     return (
-        <div className="border h-12 ">
+        <div className="border h-12 flex justify-between">
             <h1 className="ml-10 font-bold text-2xl font-sans">Interview Buddy</h1>
+            <DarkModeToggle/>
         </div>
     )
 }
