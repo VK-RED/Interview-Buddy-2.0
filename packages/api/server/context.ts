0cf735fc5886700be2db8a807cca3a5a51abc8b7
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-05 09:42:00 +0530

    fix: initialisation of multiple PrismaClients

--- packages/api/server/context.ts
+++ packages/api/server/context.ts
@@ -1,12 +1,11 @@
 
 import { CreateNextContextOptions } from "@trpc/server/adapters/next";
-import { PrismaClient } from "db"
+import { prisma } from "db"
 import { getServerSession } from "auth";
 import { authOptions } from "auth";
 
 export const createContext = async (opts: CreateNextContextOptions) =>{
     const session = await getServerSession(opts.req, opts.res, authOptions);
-    const prisma = new PrismaClient();
     return {prisma,session};
 }
 
