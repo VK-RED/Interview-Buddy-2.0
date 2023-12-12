2738af50aa55775fc3a7b8f79e6306d0a4defc4d
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-12 10:21:47 +0530

    customize the signin page

--- packages/ui/createAccount.tsx
+++ packages/ui/createAccount.tsx
@@ -22,7 +22,7 @@ export function CreateAccount(){
             <CardHeader className="space-y-1">
                 <CardTitle className="text-2xl">Create an account</CardTitle>
                 <CardDescription>
-                Enter your email below to create your account
+                Click the button below to create an account
                 </CardDescription>
             </CardHeader>
             <CardContent className="grid gap-4">
@@ -32,28 +32,28 @@ export function CreateAccount(){
                     Google
                 </Button>
                 </div>
-                <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                    <span className="w-full border-t" />
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                    <span className="bg-background px-2 text-muted-foreground">
-                    Or continue with
-                    </span>
-                </div>
-                </div>
-                <div className="grid gap-2">
+                {/* <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                        <span className="w-full border-t" />
+                    </div>
+                    <div className="relative flex justify-center text-xs uppercase">
+                        <span className="bg-background px-2 text-muted-foreground">
+                        Or continue with
+                        </span>
+                    </div>
+                </div> */}
+                {/* <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
                 <Input id="email" type="email" placeholder="me@example.com" />
                 </div>
                 <div className="grid gap-2">
                 <Label htmlFor="password">Password</Label>
                 <Input id="password" type="password" />
-                </div>
+                </div> */}
             </CardContent>
-            <CardFooter>
+            {/* <CardFooter>
                 <Button className="w-full">Create account</Button>
-            </CardFooter>
+            </CardFooter> */}
         </Card>
     )
 }
