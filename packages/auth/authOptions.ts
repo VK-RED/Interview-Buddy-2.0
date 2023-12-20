65cc1b5246c4301375f2f7fb4983af577243153a
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-20 09:18:34 +0530

    setup credentials provider

--- packages/auth/authOptions.ts
+++ packages/auth/authOptions.ts
@@ -3,7 +3,7 @@ import { SessionStrategy } from "next-auth";
 import {PrismaAdapter} from "@auth/prisma-adapter"
 import {PrismaClient} from "@prisma/client"
 import GoogleProvider from "next-auth/providers/google";
-
+import CredentialsProvider from "next-auth/providers/credentials"
 const prisma = new PrismaClient()
 
 export const authOptions = {
@@ -16,7 +16,41 @@ export const authOptions = {
         clientId: process.env.GOOGLE_ID as string,
         clientSecret: process.env.GOOGLE_SECRET as string, 
       }),
-      // ...add more providers here
+      CredentialsProvider({
+        // The name to display on the sign in form (e.g. 'Sign in with...')
+        name: 'Credentials',
+        // The credentials is used to generate a suitable form on the sign in page.
+        // You can specify whatever fields you are expecting to be submitted.
+        // e.g. domain, username, password, 2FA token, etc.
+        // You can pass any HTML attribute to the <input> tag through the object.
+        credentials: {
+          email: { label: "Email", type: "email", placeholder: "adithiyan@gmail.com" },
+          password: { label: "Password", type: "password" }
+        },
+        async authorize(credentials, req) {
+          // You need to provide your own logic here that takes the credentials
+          // submitted and returns either a object representing a user or value
+          // that is false/null if the credentials are invalid.
+          // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+          // You can also use the `req` object to obtain additional parameters
+          // (i.e., the request IP address)
+          
+
+          if(!credentials?.email || !credentials.password) return null;
+
+          //Handle Signin here 
+          
+          const user = await prisma.user.findUnique({where:{
+            email:credentials.email,
+          }})
+          
+          if(!user || user.password !== credentials.password) return  null;
+          
+          return user;
+          
+        }
+      })
+      
     ],
     secret: process.env.NEXTAUTH_SECRET as string,
     session: {
@@ -25,8 +59,8 @@ export const authOptions = {
       // If you use an `adapter` however, we default it to `"database"` instead.
       // You can still force a JWT session by explicitly defining `"jwt"`.
       // When using `"database"`, the session cookie will only contain a `sessionToken` value,
-      // which is used to look up the session in the database.
-      strategy: "database" as SessionStrategy,
+      // which is used to look up the session in the database.return {...newUser};
+      strategy: "jwt" as SessionStrategy,
     
       // Seconds - How long until an idle session expires and is no longer valid.
       maxAge: 30 * 24 * 60 * 60, // 30 days
@@ -53,6 +87,26 @@ export const authOptions = {
         return url.startsWith(baseUrl)
           ? Promise.resolve(baseUrl+"/topics")
           : Promise.resolve(baseUrl)
+      },
+
+      async jwt({ token, account, profile, session }: any) {
+        
+        /*
+          This function is fired whenever a new token is created , ideally during signin
+          token will be an object containing user details passed to session({token})
+        */
+
+        return token;
+      },
+
+      async session({session,token,user}:any) {
+          
+        /*
+          Any extra info, needed to pass to the client can be done here. Ex: userId to the client 
+          The session and token are got from the jwt()
+        */
+          
+          return session;
       }
     }
   }
--- packages/ui/createAccount.tsx
+++ packages/ui/createAccount.tsx
@@ -12,17 +12,54 @@ import {
   } from "./src/ui/card"
 
 import { authAtomSelector, useRecoilValue } from "store"
+import { useState } from "react"
+import { useSearchParams } from "next/navigation"
+import { useRouter } from "next/router"
+import { useToast } from "./src/ui/use-toast"
 
 export function CreateAccount(){
 
     const {signIn} = useRecoilValue(authAtomSelector);
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const {toast} = useToast();
+    const callbackUrl = searchParams.get('callbackUrl')||"/";
+
+    
+
+    const handleAuth = async()=>{
+        const res = await signIn('credentials',{
+            redirect: false,
+            email,
+            password,
+            callbackUrl,
+        })
+
+        setEmail(""),
+        setPassword("");
+
+        if(res?.error){
+            toast({description:"Please enter valid email or password",variant:"destructive"})
+            setTimeout(() => {
+                router.push("/");
+            }, 2000);
+            
+            return;
+        }
+        else{
+            router.push(callbackUrl);
+        }
+    }
 
     return(
         <Card>
             <CardHeader className="space-y-1">
-                <CardTitle className="text-2xl">Create an account</CardTitle>
+                <CardTitle className="text-2xl">Login to your account</CardTitle>
                 <CardDescription>
-                Click the button below to create an account
+                Click the button below to Login
                 </CardDescription>
             </CardHeader>
             <CardContent className="grid gap-4">
@@ -32,7 +69,7 @@ export function CreateAccount(){
                     Google
                 </Button>
                 </div>
-                {/* <div className="relative">
+                <div className="relative">
                     <div className="absolute inset-0 flex items-center">
                         <span className="w-full border-t" />
                     </div>
@@ -41,19 +78,19 @@ export function CreateAccount(){
                         Or continue with
                         </span>
                     </div>
-                </div> */}
-                {/* <div className="grid gap-2">
+                </div>
+                <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="me@example.com" />
+            <Input id="email" type="email" placeholder="adithiyan@gmail.com" value={email} onChange={(e)=>setEmail((em)=>e.target.value)}/>
                 </div>
                 <div className="grid gap-2">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" />
-                </div> */}
+                <Input id="password" type="password" value={password} onChange={(e)=>setPassword((p)=>e.target.value)} placeholder="NJKNsdc@123"/>
+                </div>
             </CardContent>
-            {/* <CardFooter>
-                <Button className="w-full">Create account</Button>
-            </CardFooter> */}
+            <CardFooter>
+                <Button className="w-full" onClick={handleAuth}>Login</Button>
+            </CardFooter>
         </Card>
     )
 }
