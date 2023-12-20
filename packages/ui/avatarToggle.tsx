ad274ddb785700fd71270607a4d334a28d52d83b
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-20 14:30:34 +0530

    add avatar fallback image

--- packages/ui/avatarToggle.tsx
+++ packages/ui/avatarToggle.tsx
@@ -1,25 +1,15 @@
 "use client"
 
 import * as React from "react"
-
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "ui/src/ui/dropdown-menu"
-
 import { Avatar, AvatarFallback, AvatarImage } from "./src/ui/avatar";
-
 import { useRecoilValue, authAtomSelector } from "store";
 
-// type AvatarTogglerType = {
-//     profilePic : string,
-//     status: "authenticated"|"unauthenticated"|"loading",
-//     signIn:()=>{},
-//     signOut:()=>{},
-// }
-// {status,profilePic,signIn,signOut}:AvatarTogglerType
 export function AvatarToggle(){
 
   const {signIn, signOut, useSession} = useRecoilValue(authAtomSelector)
@@ -30,7 +20,9 @@ export function AvatarToggle(){
         <DropdownMenuTrigger asChild>
             <Avatar className="cursor-pointer">
                 <AvatarImage src={session?.user?.image||""} alt="@shadcn" />
-                <AvatarFallback>CN</AvatarFallback>
+                <AvatarFallback>
+                  <img src="https://github.com/shadcn.png" alt="@shadcn" />
+                </AvatarFallback>
             </Avatar>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
