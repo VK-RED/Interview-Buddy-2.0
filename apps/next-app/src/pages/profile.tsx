d3d681abc606ecccd396932cb736c40da3c137cd
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-23 08:30:28 +0530

    feature: get total chats and conversations

--- apps/next-app/src/pages/profile.tsx
+++ apps/next-app/src/pages/profile.tsx
@@ -1,8 +1,70 @@
+import { useSession } from "auth"
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { Loader, Navbar } from "ui";
+import { trpc } from "../utils/trpc";
+import { useToast } from "ui";
 export default function Profile(){
+
+    const {data:session, status} = useSession();
+    const router = useRouter();
+    const {toast} = useToast();
+
+    //Get the Total Convos
+    const convoData = trpc.meta.countTotalConvos.useQuery({},{
+        enabled:false,
+        onSuccess(data) {
+            console.log(data);
+        },
+        onError(err) {
+            toast({description:"Something went wrong with getting total conversations",variant:"destructive"});
+        },
+
+    })
+
+    //Get the Total Chats
+    const chatData = trpc.meta.countChats.useQuery({},{
+        enabled:false,
+        onSuccess(data) {
+            console.log(data);
+        },
+        onError(err) {
+            toast({description:"Something went wrong with getting total chats",variant:"destructive"});
+        },
+    })
+
+    useEffect(()=>{
+        if(status === "unauthenticated"){
+            router.push("/");
+        }
+        else if(status === "authenticated"){
+            convoData.refetch();
+            chatData.refetch();
+        }
+    },[status])
 
     return (
         <div>
-            I am the profile Page
+            {
+                status === "loading" 
+                ? <Loader /> :
+
+                <div className="min-w-[500px] dark:bg-black h-screen">
+                    <Navbar />
+
+                    <div className="mt-20 flex flex-col items-center">
+
+                        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                            Profile
+                        </h1>
+
+
+                    </div>
+                    
+
+
+                </div>
+            }
         </div>
     )
 }
