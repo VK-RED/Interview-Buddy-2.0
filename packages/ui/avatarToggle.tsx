822eda29364be25797e4edecbd4db8e076036ecb
Vishnu Kumar <vishnukumart96@gmail.com>
2023-12-23 15:25:52 +0530

    add profile button in avatar toggle

--- packages/ui/avatarToggle.tsx
+++ packages/ui/avatarToggle.tsx
@@ -5,16 +5,19 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "ui/src/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from "./src/ui/avatar";
 import { useRecoilValue, authAtomSelector } from "store";
+import { useRouter } from "next/navigation";
 
 export function AvatarToggle(){
 
+  const router = useRouter();
   const {signIn, signOut, useSession} = useRecoilValue(authAtomSelector)
   const {data:session,status} = useSession();
-
+  
     return(
         <DropdownMenu>
         <DropdownMenuTrigger asChild>
@@ -26,6 +29,13 @@ export function AvatarToggle(){
             </Avatar>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
+          <DropdownMenuItem className="cursor-pointer"
+            onClick={()=>{
+              router.push("/profile")
+            }}>
+            Profile
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
           <DropdownMenuItem className="cursor-pointer"
            onClick={()=>{
             if(status === "unauthenticated"){
